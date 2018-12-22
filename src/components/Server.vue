26b593a323e5fd0257f024ecc20563b836c90a89
Guillaume Gérard <guillaume.gerard88@gmail.com>
2018-12-22 22:56:41 +0100

    add copy icon in server detail

--- src/components/Server.vue
+++ src/components/Server.vue
@@ -1,6 +1,11 @@
 <template>
   <tr :class="trClasses" v-if="checkOnline.status >= 0">
-    <td>{{server.ip}}:{{server.port}}</td>
+    <td>
+      <span class="fullAddress">{{fullAddress}}</span>
+      <button class="button margin-left" data-tooltip="Copy">
+        <img alt="Copy" class="icon" src="../assets/copy.png" data-copy-selector=".fullAddress" v-on:click="copy">
+      </button>
+    </td>
     <td :data-tooltip="infos">
       {{checkOnline.data.online}}
       <img alt="Users" class="icon" src="../assets/users.png">
@@ -27,6 +32,9 @@ export default {
       this.$store.dispatch("checkServer", this.server.ip, this.server.port);
       return this.server;
     },
+    fullAddress() {
+      return `${this.server.ip}:${this.server.port}`;
+    },
     infos() {
       return `Version: ${this.server.data.version}`;
     },
@@ -34,7 +42,19 @@ export default {
       return this.server.flag.toUpperCase();
     },
     trClasses() {
-      return this.server.highlight ? 'highlight': '';
+      return this.server.highlight ? "highlight" : "";
+    }
+  },
+  methods: {
+    copy(event) {
+      let el = this.$el.querySelector(
+        event.target.getAttribute("data-copy-selector")
+      );
+      let range = document.createRange();
+      range.selectNode(el);
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      window.getSelection().removeAllRanges();
     }
   }
 };
@@ -44,6 +64,26 @@ export default {
 tr.highlight {
   td {
     font-weight: bold;
+  }
+}
+.margin-left {
+  margin-left: 4px;
+}
+.button {
+  padding: 4px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background-color: #ffffff00;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #52525220;
+  }
+
+  &:active {
+    background-color: #52525240;
+    transform: translateY(1px);
   }
 }
 </style>
