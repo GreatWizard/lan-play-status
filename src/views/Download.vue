c2fa2c6de7cf9fd83950fadbfa7d90542042fe96
Guillaume Gérard <guillaume.gerard88@gmail.com>
2018-12-22 12:28:37 +0100

    update download page

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="download">
     <h1>Download</h1>
-    <h2>Ldn_mtm v0.1.3</h2>
+    <h2>LAN Play Client 0.0.7</h2>
     <p>
-      <a href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v0.1.3/ldn_mitm_v0.1.3.kip">
-        Ldn_mtm v0.1.3 Atmosphere 8.0+
-      </a>
-    </p>
-    <p>
-      <a href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v0.1.3_ReiNX/ldn_mitm_v0.1.3_ReiNX.kip" target="=_blank">
-       Ldn_mtm v0.1.3 ReiNX
-      </a>
-    </p>
-    <h2>LAN Play Client 0.0.6</h2>
-    <p>
-      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.6/lan-play-win64.exe" target="=_blank">
+      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.7/lan-play-win64.exe" target="=_blank">
         Windows (64bit)
       </a>
     </p>
     <p>
-      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.6/lan-play-win32.exe" target="=_blank">
+      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.7/lan-play-win32.exe" target="=_blank">
         Windows (32bit)
       </a>
     </p>
     <p>
-      <a href="https://mega.nz/#!CZBHQaKR!9HT4GO-T0Fb9eXn4EO07o21WCJlcxadddqeL_gyf9p0" target="=_blank">
+      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.7/lan-play-macos" target="=_blank">
         MacOS
       </a>
     </p>
     <p>
-      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.6/lan-play-linux" target="=_blank">
+      <a href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.7/lan-play-linux" target="=_blank">
         Linux
       </a>
+    </p>
+    <h2>Switch Sysmodule (ldn_mtm)</h2>
+    <p>
+      <a href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v0.2.2/ldn_mitm_v0.2.2.kip" target="=_blank">
+        Ldn_mtm v0.2.2 for Atmosphere 8.0+
+      </a>
+    </p>
+    <p>
+      <a href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v0.1.3_ReiNX/ldn_mitm_v0.1.3_ReiNX.kip" target="=_blank">
+       Ldn_mtm v0.1.3 for ReiNX
+      </a>
+    </p>
+    <p>
+       or you can use SX-OS v2.5+
     </p>
   </div>
 </template>
