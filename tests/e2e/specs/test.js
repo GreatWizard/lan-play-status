7cabffd5cd6c5a912a4fc4db4872f457310deca1
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-01-04 21:00:22 +0100

    update dependencies

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Lan Play Status");
+    cy.contains("h1", "LAN Play Status");
   });
 });
