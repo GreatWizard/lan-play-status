fc5210059008ebb1e6e38b36527566f854c389f4
Cubuss <9220880+Cubuss@users.noreply.github.com>
2019-09-24 13:45:04 +0200

    Update releases

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="download">
     <h1>Download</h1>
-    <h2>LAN Play Client 0.1.0</h2>
+    <h2>LAN Play Client v0.2.1</h2>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-win64.exe"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.1/lan-play-win64.exe"
         target="=_blank"
       >
         Windows (64bit)
@@ -12,7 +12,7 @@
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-win32.exe"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.1/lan-play-win32.exe"
         target="=_blank"
       >
         Windows (32bit)
@@ -20,15 +20,7 @@
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-macos"
-        target="=_blank"
-      >
-        MacOS
-      </a>
-    </p>
-    <p>
-      <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-linux"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.1/lan-play-linux"
         target="=_blank"
       >
         Linux
@@ -37,18 +29,18 @@
     <h2>Switch Sysmodule and Homebrew (ldn_mitm)</h2>
     <p>
       <a
-        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.3/ldn_mitm_v1.2.3.kip"
+        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.6/ldn_mitm_v1.2.6.kip"
         target="=_blank"
       >
-        Ldn_mtm v1.2.3
+        Ldn_mtm v1.2.5
       </a>
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.3/ldnmitm_config.nro"
+        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.6/ldnmitm_config.nro"
         target="=_blank"
       >
-        Ldn_mtm config homebrew v1.2.3
+        Ldn_mtm config homebrew v1.2.6
       </a>
     </p>
     <em> (or you can use SX-OS v2.5+) </em>
