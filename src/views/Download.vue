6451e61ab691d1b7692f8dda5503d2b7e78be768
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-10-01 11:15:24 +0200

    Update download links

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,54 +1,54 @@
 <template>
   <div class="download">
     <h1>Download</h1>
-    <h2>LAN Play Client 0.1.0</h2>
+    <h2>LAN Play Client</h2>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-win64.exe"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.1/lan-play-win64.exe"
         target="=_blank"
       >
-        Windows (64bit)
+        Windows (64bit) v0.2.1
       </a>
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-win32.exe"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.1/lan-play-win32.exe"
         target="=_blank"
       >
-        Windows (32bit)
+        Windows (32bit) v0.2.1
       </a>
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-macos"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.0.7/lan-play-macos"
         target="=_blank"
       >
-        MacOS
+        MacOS v0.0.7
       </a>
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.1.0/lan-play-linux"
+        href="https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.1/lan-play-linux"
         target="=_blank"
       >
-        Linux
+        Linux v0.2.1
       </a>
     </p>
     <h2>Switch Sysmodule and Homebrew (ldn_mitm)</h2>
     <p>
       <a
-        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.3/ldn_mitm_v1.2.3.kip"
+        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.6/ldn_mitm_v1.2.6.kip"
         target="=_blank"
       >
-        Ldn_mtm v1.2.3
+        Ldn_mtm v1.2.6
       </a>
     </p>
     <p>
       <a
-        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.3/ldnmitm_config.nro"
+        href="https://github.com/spacemeowx2/ldn_mitm/releases/download/v1.2.6/ldnmitm_config.nro"
         target="=_blank"
       >
-        Ldn_mtm config homebrew v1.2.3
+        Ldn_mtm config homebrew v1.2.6
       </a>
     </p>
     <em> (or you can use SX-OS v2.5+) </em>
