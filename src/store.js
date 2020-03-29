07ddcb021a9fc55cf2b6de94dcbae9bc99c59ee5
Guillaume Gérard <guillaume.gerard88@gmail.com>
2020-03-29 23:27:45 +0200

    load servers json list directly

--- src/store.js
+++ src/store.js
@@ -1,8 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import servers from "@/servers";
+import serversSource from "../public/data/servers.json";
 
 Vue.use(Vuex);
+
+const servers = serversSource.map(
+  ({ ip, flag, port = 11451, platform = "switch", type = "node" }) => {
+    return {
+      ip,
+      port,
+      flag,
+      platform,
+      type,
+      status: undefined,
+      ping: undefined,
+      data: undefined,
+      highlight: ip === "switch.lan-play.com"
+    };
+  }
+);
 
 export default new Vuex.Store({
   state: {
