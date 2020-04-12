20e3f180ccfcc112f08f90008846290da750a760
Guillaume Gérard <guillaume.gerard88@gmail.com>
2020-04-12 18:13:26 +0200

    add main discord link on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="home">
     <img alt="Logo" class="logo" src="../assets/logo.png" />
-    <h1>LAN Play Status</h1>
+    <div>
+      <h1 class="title">
+        LAN Play Status
+        <a
+          href="https://discord.gg/zEMCu5n"
+          target="=_blank"
+          rel="noreferrer noopener"
+        >
+          <img :src="require(`@/assets/discord.svg`)" alt="Discord" />
+        </a>
+      </h1>
+    </div>
     <Servers />
   </div>
 </template>
@@ -17,6 +28,13 @@ export default {
 </script>
 
 <style lang="scss">
+.title {
+  img {
+    vertical-align: middle;
+    width: 44px;
+    height: 44px;
+  }
+}
 .logo {
   height: 8vh;
   max-height: 100px;
