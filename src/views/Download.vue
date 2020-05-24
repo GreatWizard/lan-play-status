f667f5bf79f1bad9b7fc056fd8abf6a62ca30590
Guillaume Gérard <guillaume.gerard88@gmail.com>
2020-05-24 12:08:10 +0200

    feat: add backups link for lanplay client macos own build

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -21,13 +21,38 @@
       </a>
     </p>
     <p>
+      MacOS v0.2.1:
       <a
         href="https://www111.zippyshare.com/d/2DNOz1XM/27792/lan-play-macos-v0.2.1"
         target="=_blank"
         rel="noreferrer noopener"
+        >zippyshare.com</a
       >
-        MacOS v0.2.1
-      </a>
+      &mdash;
+      <a
+        href="http://dl.free.fr/nGyzMkHje"
+        target="=_blank"
+        rel="noreferrer noopener"
+        >dl.free.fr</a
+      >
+      &mdash;
+      <a
+        href="https://1fichier.com/?dwcwk24w7g91y0cyh1w3"
+        target="=_blank"
+        rel="noreferrer noopener"
+        >1fichier.com</a
+      >
+      &mdash;
+      <a
+        href="https://uptobox.com/kvsee5axaw8k"
+        target="=_blank"
+        rel="noreferrer noopener"
+        >uptobox.com</a
+      >
+      &mdash;
+      <a href="http://ul.to/70cmx7bj" target="=_blank" rel="noreferrer noopener"
+        >uploaded.net</a
+      >
     </p>
     <p>
       <a
