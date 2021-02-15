5a5ff0ba71a1eea42075c19679b30bd67eb0fe6c
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-02-15 12:25:17 +0100

    chore: add sitemap.xml file for SEO

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,40 @@
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin({
+        base: "http://www.lan-play.com",
+        paths: [
+          {
+            path: "/",
+            priority: 1.0,
+          },
+          {
+            path: "/lobbies",
+          },
+          {
+            path: "/games",
+          },
+          {
+            path: "/download",
+          },
+          {
+            path: "/install",
+            priority: 1.0,
+          },
+          {
+            path: "/about",
+          },
+        ],
+        options: {
+          filename: "sitemap.xml",
+          lastmod: new Date().toISOString().slice(0, 10),
+          priority: 0.8,
+          changefreq: "hourly",
+        },
+      }),
+    ],
+  },
 };
