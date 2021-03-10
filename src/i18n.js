2db1aa8afe4e6de0bfb544aca32d984334acb493
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-03-10 22:00:53 +0100

    fix(i18n): push en source on tx for master branch only and detect navigator language

--- src/i18n.js
+++ src/i18n.js
@@ -25,7 +25,7 @@ function loadLocaleMessages() {
 
 export default createI18n({
   legacy: false,
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
+  locale: navigator.language || process.env.VUE_APP_I18N_LOCALE || "en",
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
   messages: loadLocaleMessages(),
   globalInjection: true
