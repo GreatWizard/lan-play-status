927b4bb76f3b9b58d68a3d4fd0f08b61ae8dc4eb
driz <40674481+drizuid@users.noreply.github.com>
2021-03-25 09:07:53 -0400

    reorg changes
    move step 6 to 3, combine original steps 3 and 4 into step 4, add graphic

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -40,7 +40,44 @@
     Finally in a terminal: <code>sudo ./lan-play-linux</code> (ensuring you are
     in the proper directory (likely ~/Downloads)
   </p>
-  <h2>{{ $t("install.games.title", { prefix: "3 - " }) }}</h2>
+    <p>
+      <img
+        alt="linux instructional"
+        class="screenshot"
+        :src="require(`@/assets/switch/lindldrun.jpg`)"
+      />
+    </p>
+      <h2>{{ $t("pages.installSwitch.test.title", { prefix: "3 - " }) }}</h2>
+    <p>
+      You can test if you have setup everything correctly by putting your Switch
+      to sleep and waking it up.
+    </p>
+    <p>
+      If everything is good, you should see IConnection on your lan-play client.
+      It will look like:
+    </p>
+    <p>
+      <img
+        alt="IConnection"
+        class="screenshot"
+        :src="require(`@/assets/switch/iconnection.jpg`)"
+      />
+    </p>
+    <p>
+      If you do not, verify your switch settings, ensure there is no autoconnect
+      profile for a different wireless profile but that autoconnect
+      <b>IS</b> enabled for the lanplay profile, and try playing a game. Failing
+      this, try rebooting your switch.
+      <strong>
+        Failing the internet and/or DNS connectivity test on your switch
+        <b>DOES NOT MATTER</b> and can be ignored.
+      </strong>
+    </p>
+    <p>
+      If this continues to fail, join #help on the lan-play discord for further
+      assistance.
+    </p>
+  <h2>{{ $t("install.games.title", { prefix: "4 - " }) }}</h2>
   <p>
     The application will prompt you to enter the relay server address. You can
     choose the server you want, like <code>switch.lan-play.com:11451</code>.
@@ -50,7 +87,6 @@
     firewall. If the client exits right away, ensure you are running as an
     administrator and pasting in the server correctly (example above).
   </p>
-  <h2>{{ $t("install.games.title", { prefix: "4 - " }) }}</h2>
   <p>
     Launch the desired game you want to play (Must have the LAN play feature!)
   </p>
