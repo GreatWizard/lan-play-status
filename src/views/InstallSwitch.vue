f7632af5d758d9811f6e43c43649a7297990762c
driz <40674481+drizuid@users.noreply.github.com>
2021-03-25 09:02:39 -0400

    remove section 6 for reorg of guide

--- src/views/InstallSwitch.vue
+++ src/views/InstallSwitch.vue
@@ -56,36 +56,6 @@
       After that you just launch your game of choice and select local wireless
       play and you should be good to go.
     </p>
-    <h2>{{ $t("pages.installSwitch.test.title", { prefix: "6 - " }) }}</h2>
-    <p>
-      You can test if you have setup everything correctly by putting your Switch
-      to sleep and waking it up.
-    </p>
-    <p>
-      If everything is good, you should see IConnection on your lan-play client.
-      It will look like:
-    </p>
-    <p>
-      <img
-        alt="IConnection"
-        class="screenshot"
-        :src="require(`@/assets/switch/iconnection.jpg`)"
-      />
-    </p>
-    <p>
-      If you do not, verify your switch settings, ensure there is no autoconnect
-      profile for a different wireless profile but that autoconnect
-      <b>IS</b> enabled for the lanplay profile, and try playing a game. Failing
-      this, try rebooting your switch.
-      <strong>
-        Failing the internet and/or DNS connectivity test on your switch
-        <b>DOES NOT MATTER</b> and can be ignored.
-      </strong>
-    </p>
-    <p>
-      If this continues to fail, join #help on the lan-play discord for further
-      assistance.
-    </p>
   </div>
 </template>
 
