01360b4d7576ace934317676ca030530b074c79f
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-03-27 12:37:19 +0100

    update install pages

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -11,44 +11,80 @@
       WinPcap
     </a>
   </p>
-
-  <p>Then run <code>lan-play-win64.exe</code>.</p>
-  as an administrator
+  <p>Then run <code>lan-play-win64.exe</code> as an administrator.</p>
   <h3>MacOS</h3>
   <p>
     First, you need to install
-    <a href="https://brew.sh" target="_blank" rel="noreferrer noopener">
-      Homebrew</a
+    <a href="https://brew.sh/" target="_blank" rel="noreferrer noopener"
+      >Homebrew</a
     >
   </p>
-  <p>Next in a terminal: <code>brew install libpcap libuv</code></p>
-  <p>
-    Then in a terminal: Go to the directory you downloaded to (likely
-    ~/Downloads) and input <code>chmod +x lan-play-macos</code>
-  </p>
-  <p>
-    Finally in a terminal: <code>sudo ./lan-play-macos</code> (ensuring you are
-    in the proper directory (likely ~/Downloads)
-  </p>
+  <p>Then in a terminal: <code>brew install switch-lan-play</code></p>
+  <p>Finally in a terminal: <code>lan-play --pmtu 500</code></p>
+  <Terminal
+    title="user@mac: ~/"
+    content="$ brew install switch-lan-play
+==> Downloading https://homebrew.bintray.com/bottles/switch-lan-play-0.2.3.catalina.bottle.tar.gz
+==> Pouring switch-lan-play-0.2.3.catalina.bottle.tar.gz
+🍺  /usr/local/Cellar/switch-lan-play/0.2.3: 5 files, 922.4KB
+$ lan-play --pmtu 500 --relay-server-addr switch.lan-play.com:11451
+Interface not specified, opening all interfaces
+[DEBUG]: open en0 ok
+[DEBUG]: open p2p0 fail: datalink(12)
+[DEBUG]: open awdl0 ok
+[DEBUG]: open llw0 ok
+[DEBUG]: open utun0 fail: datalink(0)
+[DEBUG]: open utun1 fail: datalink(0)
+[DEBUG]: open lo0 fail: datalink(0)
+[DEBUG]: open en1 ok
+[DEBUG]: open en2 ok
+[DEBUG]: open gif0 fail: datalink(0)
+[DEBUG]: open stf0 fail: datalink(0)
+[DEBUG]: open ap1 ok
+pcap loop start
+[DEBUG]: packet init buffer 0x10ecb9bb0
+[DEBUG]: pmtu is set to 500
+Server IP: 149.91.81.201"
+  />
   <h3>Ubuntu/Debian</h3>
-  <p>In a terminal: <code>sudo apt install libpcap0.8-dev libuv1-dev</code></p>
   <p>
-    Then in a terminal: Go to the directory you downloaded to (likely
-    ~/Downloads) and input <code>chmod +x lan-play-linux</code>
+    In a terminal: Go to the directory you downloaded to (likely ~/Downloads)
+    and input <code>chmod +x lan-play-linux</code>
   </p>
   <p>
-    Finally in a terminal: <code>sudo ./lan-play-linux</code> (ensuring you are
-    in the proper directory (likely ~/Downloads)
+    Then in a terminal: <code>./lan-play-linux --pmtu 500</code> (ensuring you
+    are in the proper directory (likely ~/Downloads)
   </p>
+  <Terminal
+    title="user@linux: ~/Downloads"
+    content="$ cd ~/Downloads
+$ wget --quiet https://github.com/spacemeowx2/switch-lan-play/releases/download/v0.2.3/lan-play-linux
+$ chmod +x lan-play-linux
+$ sudo ./lan-play-linux --pmtu 500 --relay-server-addr switch.lan-play.com:11451
+Interface not specified, opening all interfaces
+[DEBUG]: open veth3b172e4 ok
+[DEBUG]: open br-f2c3fb820b84 ok
+[DEBUG]: open ensi92 ok
+[DEBUG]: open any fail: datalink(113)
+[DEBUG]: open lo fail: get all zero mac
+[DEBUG]: open nfqueue fail: datalink(228)
+[DEBUG]: open usbmon1 fail
+[DEBUG]: open usbmon2 fail
+pcap loop start
+[DEBUG]: packet init buffer 0x55b3687b58c0
+[DEBUG]: pmtu is set to 500
+Server IP: 149.91.81.201"
+  />
   <h2>{{ $t("install.games.title", { prefix: "3 - " }) }}</h2>
   <p>
-    The application will prompt you to enter the relay server address. You can
-    choose the server you want, like <code>switch.lan-play.com:11451</code>.
+    If you omit the <code>--relay-server-addr</code> option, the application
+    will prompt you to enter the relay server address. You can choose the server
+    you want, like <code>switch.lan-play.com:11451</code>.
   </p>
   <p>
     You need to run the client administratively and allow it through your
     firewall. If the client exits right away, ensure you are running as an
-    administrator and pasting in the server correctly (example above).
+    administrator and pasting in the server correctly.
   </p>
   <h2>{{ $t("install.games.title", { prefix: "4 - " }) }}</h2>
   <p>
@@ -61,7 +97,12 @@
 </template>
 
 <script>
+import Terminal from "@/components/Terminal.vue";
+
 export default {
+  components: {
+    Terminal
+  },
   props: {
     type: String
   },
--- src/views/InstallSwitch.vue
+++ src/views/InstallSwitch.vue
@@ -29,7 +29,40 @@
       />
     </p>
     <Install type="switch" />
-    <h2>{{ $t("pages.installSwitch.module.title", { prefix: "5 - " }) }}</h2>
+    <h2>{{ $t("pages.installSwitch.test.title", { prefix: "5 - " }) }}</h2>
+    <p>
+      You can test if you have setup everything correctly by putting your Switch
+      to sleep and waking it up.
+    </p>
+    <p>
+      If everything is good, you should see
+      <strong>IConnection::IConnection</strong> on your lan-play client. It will
+      look like:
+    </p>
+    <Terminal
+      title="user@linux: ~/Downloads"
+      content="[DEBUG]: IConnection::IConnection
+[DEBUG]: TcpConnection::onConnection
+[DEBUG]: ptcp->onConnect
+[DEBUG]: lwip->onClose
+[DEBUG]: IConnection::writeCallback -89
+[DEBUG]: IConnection::~IConnection"
+    />
+    <p>
+      If you do not, verify your switch settings, ensure there is no autoconnect
+      profile for a different wireless profile but that autoconnect
+      <b>IS</b> enabled for the lanplay profile, and try playing a game. Failing
+      this, try rebooting your switch.
+      <strong>
+        Failing the internet and/or DNS connectivity test on your switch
+        <b>DOES NOT MATTER</b> and can be ignored.
+      </strong>
+    </p>
+    <p>
+      If this continues to fail, join #help on the lan-play discord for further
+      assistance.
+    </p>
+    <h2>{{ $t("pages.installSwitch.module.title", { prefix: "6 - " }) }}</h2>
     <p>
       You will need to download ldn_mitm if you wish to play to play games that
       don't have the lan play feature (Local Wireless mode Games).
@@ -56,47 +89,19 @@
       After that you just launch your game of choice and select local wireless
       play and you should be good to go.
     </p>
-    <h2>{{ $t("pages.installSwitch.test.title", { prefix: "6 - " }) }}</h2>
-    <p>
-      You can test if you have setup everything correctly by putting your Switch
-      to sleep and waking it up.
-    </p>
-    <p>
-      If everything is good, you should see IConnection on your lan-play client.
-      It will look like:
-    </p>
-    <p>
-      <img
-        alt="IConnection"
-        class="screenshot"
-        :src="require(`@/assets/switch/iconnection.jpg`)"
-      />
-    </p>
-    <p>
-      If you do not, verify your switch settings, ensure there is no autoconnect
-      profile for a different wireless profile but that autoconnect
-      <b>IS</b> enabled for the lanplay profile, and try playing a game. Failing
-      this, try rebooting your switch.
-      <strong>
-        Failing the internet and/or DNS connectivity test on your switch
-        <b>DOES NOT MATTER</b> and can be ignored.
-      </strong>
-    </p>
-    <p>
-      If this continues to fail, join #help on the lan-play discord for further
-      assistance.
-    </p>
   </div>
 </template>
 
 <script>
 import InstallConfig from "@/components/InstallConfig.vue";
 import Install from "@/components/Install.vue";
+import Terminal from "@/components/Terminal.vue";
 
 export default {
   components: {
     InstallConfig,
-    Install
+    Install,
+    Terminal
   }
 };
 </script>
