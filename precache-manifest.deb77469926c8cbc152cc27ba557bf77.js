b2431253a254454c6b07339e109cfa4e2e50c4a0
GreatWizard <GreatWizard@users.noreply.github.com>
2021-03-31 21:05:35 +0000

    deploy: fec9ec78ac947f731ea792153fc905c1e9114a78

---
+++ precache-manifest.deb77469926c8cbc152cc27ba557bf77.js
@@ -0,0 +1,654 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc3c087cfb9fb2933c1593c62e8167a",
+    "url": "/BingSiteAuth.xml"
+  },
+  {
+    "revision": "be0dfdc295c71565209f574197a7c558",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "db92311bab82bddea6cc2bafaf10da03",
+    "url": "/ads.txt"
+  },
+  {
+    "revision": "572bb1cd1152a00faca448c1b9392d3d",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "b90d9e67d4b12f062323",
+    "url": "/css/app.1a97b21a.css"
+  },
+  {
+    "revision": "23da6a4c7448ad06bbe9ef0eab7fc92d",
+    "url": "/data/servers.json"
+  },
+  {
+    "revision": "750fdd4a4a7fec64fff1167655adc106",
+    "url": "/google1e48015d9e2ed063.html"
+  },
+  {
+    "revision": "c1049fdec80254219df3f061eb5b7609",
+    "url": "/img/404.c1049fde.jpg"
+  },
+  {
+    "revision": "c2fe8f19b2e098df8eaef438aea3234b",
+    "url": "/img/au.c2fe8f19.svg"
+  },
+  {
+    "revision": "0cbb217e3d0a0b5683e65030fe481823",
+    "url": "/img/banjokazooie-ssbu.0cbb217e.png"
+  },
+  {
+    "revision": "90b020e0831af55f970a3b759e71b56e",
+    "url": "/img/bayonetta-ssbu.90b020e0.png"
+  },
+  {
+    "revision": "b2729f166a93c56e7cbd9e412adfef3f",
+    "url": "/img/bg.b2729f16.svg"
+  },
+  {
+    "revision": "94111c74c64b7886dc0028e0344f024c",
+    "url": "/img/blathers.94111c74.png"
+  },
+  {
+    "revision": "523b1624daebcf80206777ce3321fea0",
+    "url": "/img/boo.523b1624.png"
+  },
+  {
+    "revision": "3aa3dc3136c29461dc17cb0e5ed2d0ca",
+    "url": "/img/bowser-kart.3aa3dc31.png"
+  },
+  {
+    "revision": "a020a0c310c7201b59dcc5b487f75e6d",
+    "url": "/img/bowser-ssbu.a020a0c3.png"
+  },
+  {
+    "revision": "fc7ed05ea5d412af0815b7ee27ad6bf8",
+    "url": "/img/bowser.fc7ed05e.png"
+  },
+  {
+    "revision": "e56705d83bab36a91715012a4041025e",
+    "url": "/img/bowserjr-ssbu.e56705d8.png"
+  },
+  {
+    "revision": "65163045dca00bf9c17d8620cbdef220",
+    "url": "/img/br.65163045.svg"
+  },
+  {
+    "revision": "b641ea7edb6df600f417a3de0cbc49ae",
+    "url": "/img/byleth-ssbu.b641ea7e.png"
+  },
+  {
+    "revision": "7898d2bd675a2d9b021804a84a5923c5",
+    "url": "/img/ca.7898d2bd.svg"
+  },
+  {
+    "revision": "55e87ccc92ceb4fef864d37df2c0afe2",
+    "url": "/img/callie.55e87ccc.png"
+  },
+  {
+    "revision": "d5d97cb618f9a0c2ebe9dd672955f2e2",
+    "url": "/img/captainfalcon-ssbu.d5d97cb6.png"
+  },
+  {
+    "revision": "56c96ced69702264220248d4a36224be",
+    "url": "/img/charizard-ssbu.56c96ced.png"
+  },
+  {
+    "revision": "fd6e2f3624748bccd34734decfbc136b",
+    "url": "/img/chrom-ssbu.fd6e2f36.png"
+  },
+  {
+    "revision": "2d653694d18766443338660e50592b78",
+    "url": "/img/cloud-ssbu.2d653694.png"
+  },
+  {
+    "revision": "c5ea0f73a37933d915329817b921c87a",
+    "url": "/img/cn.c5ea0f73.svg"
+  },
+  {
+    "revision": "2aa79a51ca9942793806ccf516c877c3",
+    "url": "/img/corrin-ssbu.2aa79a51.png"
+  },
+  {
+    "revision": "a9a7400a805e9a3e2028e594c446cc7d",
+    "url": "/img/daisy-ssbu.a9a7400a.png"
+  },
+  {
+    "revision": "b5404aae27fe108281683ee7d6565795",
+    "url": "/img/darkpit-ssbu.b5404aae.png"
+  },
+  {
+    "revision": "ca2531b3436d5e894049caa97f0cf77a",
+    "url": "/img/darksamus-ssbu.ca2531b3.png"
+  },
+  {
+    "revision": "c360f786ad05e2f97e85fdbf81703d0e",
+    "url": "/img/de.c360f786.svg"
+  },
+  {
+    "revision": "15b83d46eb37e5d7afa2a71c4b4b1141",
+    "url": "/img/diddykong-ssbu.15b83d46.png"
+  },
+  {
+    "revision": "586627d41d4fc25e47d405a1f6db6203",
+    "url": "/img/discord.586627d4.svg"
+  },
+  {
+    "revision": "ec464b3aff7c0779b99c54c0b66a9837",
+    "url": "/img/donkey-kart.ec464b3a.png"
+  },
+  {
+    "revision": "aca85cfae98c745fd8de2ad7ca1bcfb8",
+    "url": "/img/donkeykong-ssbu.aca85cfa.png"
+  },
+  {
+    "revision": "37fc5043317d638fea15f88b15243cd1",
+    "url": "/img/drmario-ssbu.37fc5043.png"
+  },
+  {
+    "revision": "1863163e31154a344e1ba294ecd2506c",
+    "url": "/img/duckhunt-ssbu.1863163e.png"
+  },
+  {
+    "revision": "35df1cce57681caa99a3312df3d4ed17",
+    "url": "/img/es.35df1cce.svg"
+  },
+  {
+    "revision": "63e852fb4476a84a86e0858f4eba0e19",
+    "url": "/img/falco-ssbu.63e852fb.png"
+  },
+  {
+    "revision": "142b0fbae6c5daa506097a42998a447c",
+    "url": "/img/felyne.142b0fba.png"
+  },
+  {
+    "revision": "edce897312e87147dadb9eb9d4a476a0",
+    "url": "/img/fox-ssbu.edce8973.png"
+  },
+  {
+    "revision": "cbf847bcc19483852ee871f42a869614",
+    "url": "/img/fr.cbf847bc.svg"
+  },
+  {
+    "revision": "0ab05cf2a09129bfb885a5646cd1bf14",
+    "url": "/img/ganondorf-ssbu.0ab05cf2.png"
+  },
+  {
+    "revision": "d950421a04ffb639bd8f5ce4a96a53af",
+    "url": "/img/greninja-ssbu.d950421a.png"
+  },
+  {
+    "revision": "3a25573bdd6112b3a65102eeeb5e10cb",
+    "url": "/img/hero-ssbu.3a25573b.png"
+  },
+  {
+    "revision": "d8c4e3fcd602966afa94a4abf964b83c",
+    "url": "/img/hk.d8c4e3fc.svg"
+  },
+  {
+    "revision": "b3f17eafb7004c716f95bdb7c80d7bda",
+    "url": "/img/iceclimbers-ssbu.b3f17eaf.png"
+  },
+  {
+    "revision": "7943db38131dc279f90c67fb4fb0c830",
+    "url": "/img/id.7943db38.svg"
+  },
+  {
+    "revision": "76977bf6a21da9d4d07377e7e101e6c2",
+    "url": "/img/ike-ssbu.76977bf6.png"
+  },
+  {
+    "revision": "18318c1b8418dc5d29f3f9b3cae6f33b",
+    "url": "/img/incineroar-ssbu.18318c1b.png"
+  },
+  {
+    "revision": "e9033bad03053ac84b890bc62e7c5842",
+    "url": "/img/inkling-boy.e9033bad.png"
+  },
+  {
+    "revision": "5417ec7008428f9aca87028454e20fa4",
+    "url": "/img/inkling-girl.5417ec70.png"
+  },
+  {
+    "revision": "fbe49bc059229c3b233fc15ef9657da6",
+    "url": "/img/inkling-squid.fbe49bc0.png"
+  },
+  {
+    "revision": "2afa383b15544e3a46cfa1ab24778ddf",
+    "url": "/img/inkling-ssbu.2afa383b.png"
+  },
+  {
+    "revision": "ec27cd57121174ab1724105762d519b8",
+    "url": "/img/isabelle-ssbu.ec27cd57.png"
+  },
+  {
+    "revision": "bada3109e36dfe022ccc5673058c6293",
+    "url": "/img/isabelle.bada3109.png"
+  },
+  {
+    "revision": "f940133270246bf1ebf8a38d62fc9ced",
+    "url": "/img/island-code.f9401332.png"
+  },
+  {
+    "revision": "07155a2754f0469639c847e7f665dfe8",
+    "url": "/img/island.07155a27.png"
+  },
+  {
+    "revision": "958b7ae3441779a4e849605f0ac47320",
+    "url": "/img/ivysaur-ssbu.958b7ae3.png"
+  },
+  {
+    "revision": "1c22bb56d2c0112656e617753dd15d6f",
+    "url": "/img/jigglypuff-ssbu.1c22bb56.png"
+  },
+  {
+    "revision": "92f72332a90cdf434f04d72c22d04ef1",
+    "url": "/img/joker-ssbu.92f72332.png"
+  },
+  {
+    "revision": "86b1228ef1facc2249739bb057804778",
+    "url": "/img/ken-ssbu.86b1228e.png"
+  },
+  {
+    "revision": "6d57af55fea3e34e0dbe2808995479db",
+    "url": "/img/kingdedede-ssbu.6d57af55.png"
+  },
+  {
+    "revision": "711d527302446f18f35743fbd4c2aefa",
+    "url": "/img/kingkrool-ssbu.711d5273.png"
+  },
+  {
+    "revision": "33b0566448c308a0a69eaf6f343ebe81",
+    "url": "/img/kk-slider.33b05664.png"
+  },
+  {
+    "revision": "2926fc2b87eee7ff5266aa852a75e994",
+    "url": "/img/link-ssbu.2926fc2b.png"
+  },
+  {
+    "revision": "6524ca3781e8363b8be6faca7d355a49",
+    "url": "/img/littlemac-ssbu.6524ca37.png"
+  },
+  {
+    "revision": "0a5edbf4fc2107dffb40ae72aa3b6248",
+    "url": "/img/lock.0a5edbf4.png"
+  },
+  {
+    "revision": "f64272e3080b77b8c29f16b4862b6539",
+    "url": "/img/logo.f64272e3.png"
+  },
+  {
+    "revision": "1485f97a20350591051ba18213ad40d0",
+    "url": "/img/lucario-ssbu.1485f97a.png"
+  },
+  {
+    "revision": "613e926521361f9604d7ad95bdc56999",
+    "url": "/img/lucas-ssbu.613e9265.png"
+  },
+  {
+    "revision": "93a1d1f70ea1b71394208e5cab3d3d2c",
+    "url": "/img/lucina-ssbu.93a1d1f7.png"
+  },
+  {
+    "revision": "852cb91698f0a408884692f2c832e48c",
+    "url": "/img/luigi-kart.852cb916.png"
+  },
+  {
+    "revision": "fdfba0723a9af0c38fa0ed031d008936",
+    "url": "/img/luigi-mansion.fdfba072.png"
+  },
+  {
+    "revision": "b3800c1fa05cb353cae8709d6af70b4d",
+    "url": "/img/luigi-ssbu.b3800c1f.png"
+  },
+  {
+    "revision": "37202b173e697a5effb9dd5211eced07",
+    "url": "/img/luigi.37202b17.png"
+  },
+  {
+    "revision": "17f13741e01be6f935bf566233e17495",
+    "url": "/img/marie.17f13741.png"
+  },
+  {
+    "revision": "871140961769aa652fa2169deeb07612",
+    "url": "/img/mario-kart.87114096.png"
+  },
+  {
+    "revision": "6751b7467eb0be59326c9c66fcfff752",
+    "url": "/img/mario-ssbu.6751b746.png"
+  },
+  {
+    "revision": "b21304ac7df36744cf27e1276a79dbc3",
+    "url": "/img/mario.b21304ac.png"
+  },
+  {
+    "revision": "2c151e693bc90c87d5f9ab3b74faa1f4",
+    "url": "/img/marth-ssbu.2c151e69.png"
+  },
+  {
+    "revision": "666812498f45a8900c924048a048b451",
+    "url": "/img/megaman-ssbu.66681249.png"
+  },
+  {
+    "revision": "5e0ab152081fcc9027857eeefc0f6971",
+    "url": "/img/melynx.5e0ab152.png"
+  },
+  {
+    "revision": "8c25efcbd495a20501e35ba355c98628",
+    "url": "/img/metaknight-ssbu.8c25efcb.png"
+  },
+  {
+    "revision": "d52758409100a99e61aa4a31677b8478",
+    "url": "/img/mewtwo-ssbu.d5275840.png"
+  },
+  {
+    "revision": "7e07568bda724b4588e33bc776709b0d",
+    "url": "/img/mh-ticket.7e07568b.png"
+  },
+  {
+    "revision": "dfbc3bdb6b5a633031f792cc6680b22c",
+    "url": "/img/miifighter-ssbu.dfbc3bdb.png"
+  },
+  {
+    "revision": "9f534a4098f961c5f5d42c49b847d4f4",
+    "url": "/img/minmin-ssbu.9f534a40.png"
+  },
+  {
+    "revision": "d76e444f3bbabf0d2db9cd0a17c5630a",
+    "url": "/img/mythra-ssbu.d76e444f.png"
+  },
+  {
+    "revision": "6e50b5bc50884d1ae5316f89b4a3973e",
+    "url": "/img/ness-ssbu.6e50b5bc.png"
+  },
+  {
+    "revision": "5ed1206794d5a6d5266a14e8b619e35e",
+    "url": "/img/nl.5ed12067.svg"
+  },
+  {
+    "revision": "c97c7c1d9fbd3c133c28e8160703faff",
+    "url": "/img/no-cover.c97c7c1d.jpg"
+  },
+  {
+    "revision": "db3e5e0b2b488bfffd3f230d75b473dd",
+    "url": "/img/olimar-ssbu.db3e5e0b.png"
+  },
+  {
+    "revision": "7d5c0fd02c28dc3944e177b342cc1177",
+    "url": "/img/pa.7d5c0fd0.svg"
+  },
+  {
+    "revision": "3c5c1ba8a64ee3f71040e1ae4bb9085b",
+    "url": "/img/palutena-ssbu.3c5c1ba8.png"
+  },
+  {
+    "revision": "a1240568febc29d94a1125f30f5b0f8c",
+    "url": "/img/peach-kart.a1240568.png"
+  },
+  {
+    "revision": "45af3d4cd926e32d3e0acf8e1df5c776",
+    "url": "/img/peach-ssbu.45af3d4c.png"
+  },
+  {
+    "revision": "217d6153e3a5310a990b2cda61065aca",
+    "url": "/img/peach.217d6153.png"
+  },
+  {
+    "revision": "5f854ae67c1d6392e128b22c1d2b926b",
+    "url": "/img/pichu-ssbu.5f854ae6.png"
+  },
+  {
+    "revision": "d6363c738edabf05efa2ebeb28a03c33",
+    "url": "/img/pikachin-kit-game-de-pirameki-daisakusen.d6363c73.jpg"
+  },
+  {
+    "revision": "2d16cf4f2387ef388832c25b851350f3",
+    "url": "/img/pikachu-ssbu.2d16cf4f.png"
+  },
+  {
+    "revision": "975d442322df7be03eff1dd96e153497",
+    "url": "/img/piranhaplant-ssbu.975d4423.png"
+  },
+  {
+    "revision": "e7a81f8a34d9302a768dd16955c7be9f",
+    "url": "/img/pit-ssbu.e7a81f8a.png"
+  },
+  {
+    "revision": "0fa73d8adb035a52f7ff6c6fd9c73743",
+    "url": "/img/pokemontrainer-ssbu.0fa73d8a.png"
+  },
+  {
+    "revision": "ad0daee7f4e427457757032b94b8378e",
+    "url": "/img/pro-yakyuu-famista-evolution.ad0daee7.jpg"
+  },
+  {
+    "revision": "31513faff6a9a5c56176bc05e2364ffc",
+    "url": "/img/pyra-ssbu.31513faf.png"
+  },
+  {
+    "revision": "1bd8445ba197ffcb92ff1f6bc64758d7",
+    "url": "/img/red-cap.1bd8445b.png"
+  },
+  {
+    "revision": "add76089a87f343aac0814aa5d01d755",
+    "url": "/img/richter-ssbu.add76089.png"
+  },
+  {
+    "revision": "a98a0db1f7c5d626185e584210619c45",
+    "url": "/img/ridley-ssbu.a98a0db1.png"
+  },
+  {
+    "revision": "264fb4444a2290802978b52770d42d4b",
+    "url": "/img/rob-ssbu.264fb444.png"
+  },
+  {
+    "revision": "71e3e72d852a6974673da8b669372551",
+    "url": "/img/robin-ssbu.71e3e72d.png"
+  },
+  {
+    "revision": "2ae7c1aa45442c068f605d39aa4178d7",
+    "url": "/img/rosalina-kart.2ae7c1aa.png"
+  },
+  {
+    "revision": "d844471cf330c50626b0d1ee9bc94976",
+    "url": "/img/rosalina-ssbu.d844471c.png"
+  },
+  {
+    "revision": "dbc6b4e68519b0a6ecc56d78c59c01a2",
+    "url": "/img/rosalina.dbc6b4e6.png"
+  },
+  {
+    "revision": "26c858cf0984d300d67deb60ac0934d0",
+    "url": "/img/roy-ssbu.26c858cf.png"
+  },
+  {
+    "revision": "a88ec6cd17bcbf5b33e5a94d4583408b",
+    "url": "/img/ru.a88ec6cd.svg"
+  },
+  {
+    "revision": "dc392346a45b6bd3b04176a2f45c86c0",
+    "url": "/img/ryu-ssbu.dc392346.png"
+  },
+  {
+    "revision": "527d691c18975d3db05174d5ed39a9d6",
+    "url": "/img/samus-ssbu.527d691c.png"
+  },
+  {
+    "revision": "c1690479f3c26f6a4d76395b68b57be3",
+    "url": "/img/sephiroth-ssbu.c1690479.png"
+  },
+  {
+    "revision": "1f6f8ca195434dca1bff1200aa580567",
+    "url": "/img/settings1.1f6f8ca1.jpg"
+  },
+  {
+    "revision": "9b0ae184c44db4cb7013aee6f18cbf7c",
+    "url": "/img/settings1.9b0ae184.jpg"
+  },
+  {
+    "revision": "1035d313c85172f60e0f8fbfe1f2602e",
+    "url": "/img/settings2.1035d313.jpg"
+  },
+  {
+    "revision": "efbf7c05accd3d1b9fd3bc9ee94cedf4",
+    "url": "/img/settings2.efbf7c05.jpg"
+  },
+  {
+    "revision": "299547557193614ad4fa1823fa6df4a8",
+    "url": "/img/settings3.29954755.jpg"
+  },
+  {
+    "revision": "3be79b02556dc8f5d3c807be0a3d3a79",
+    "url": "/img/settings3.3be79b02.jpg"
+  },
+  {
+    "revision": "18b3948f4312ac385e86adfaa8054c79",
+    "url": "/img/settings4.18b3948f.jpg"
+  },
+  {
+    "revision": "c09b2c0f44d94e5e0b5c217f1ee30617",
+    "url": "/img/sheik-ssbu.c09b2c0f.png"
+  },
+  {
+    "revision": "b68262b034e480f3f66968cbaba60e4b",
+    "url": "/img/shulk-ssbu.b68262b0.png"
+  },
+  {
+    "revision": "7be60caedc648b71624750c4c5ebd666",
+    "url": "/img/simon-ssbu.7be60cae.png"
+  },
+  {
+    "revision": "1bcd8904cf5161231217bf88c2113d7b",
+    "url": "/img/snake-ssbu.1bcd8904.png"
+  },
+  {
+    "revision": "908f814333b9b85f34c46f068119499e",
+    "url": "/img/sonic-ssbu.908f8143.png"
+  },
+  {
+    "revision": "b42176e34846e4ce9cd75ece6fcda17d",
+    "url": "/img/steve-ssbu.b42176e3.png"
+  },
+  {
+    "revision": "a3a839fe9a8632cde810c83592825385",
+    "url": "/img/switch.a3a839fe.png"
+  },
+  {
+    "revision": "0ad3dc613235a03e25e0777aa436036f",
+    "url": "/img/terry-ssbu.0ad3dc61.png"
+  },
+  {
+    "revision": "859085de78d86601f6afa892f10d2fa1",
+    "url": "/img/timmy.859085de.png"
+  },
+  {
+    "revision": "b1af4b4ba09ac9a148d6d0fab2d3bb82",
+    "url": "/img/toad-kart.b1af4b4b.png"
+  },
+  {
+    "revision": "44514c83ee88c03dc3025c23a68a5508",
+    "url": "/img/toad.44514c83.png"
+  },
+  {
+    "revision": "0a411df2b793b3c86e5c95bd243f94e8",
+    "url": "/img/tom-nook.0a411df2.png"
+  },
+  {
+    "revision": "4e3cabc9b74ead8b6d727b622d6269dd",
+    "url": "/img/tommy.4e3cabc9.png"
+  },
+  {
+    "revision": "1537e773efa34cb16cdcda25ef734a3c",
+    "url": "/img/toonlink-ssbu.1537e773.png"
+  },
+  {
+    "revision": "8862e0af428dfac076409093366c8dcb",
+    "url": "/img/tw.8862e0af.svg"
+  },
+  {
+    "revision": "290ea9221cc60f0a8a1123932d6294c9",
+    "url": "/img/us.290ea922.svg"
+  },
+  {
+    "revision": "ac6e53b55b4b84b907ca7e19a916cc86",
+    "url": "/img/villager-ssbu.ac6e53b5.png"
+  },
+  {
+    "revision": "af7135301e3a2be86bcbce39412d48ab",
+    "url": "/img/vn.af713530.svg"
+  },
+  {
+    "revision": "1d30eda0ca4dae30b0365d5b0a36eccc",
+    "url": "/img/wario-kart.1d30eda0.png"
+  },
+  {
+    "revision": "35cb74014b8ae50e45fa82a1cd5a13f9",
+    "url": "/img/wario-ssbu.35cb7401.png"
+  },
+  {
+    "revision": "eafe62b53d5c62f516e4fb29de2553da",
+    "url": "/img/whatsapp.eafe62b5.svg"
+  },
+  {
+    "revision": "c366f8b032e76aa23727117ee9510c6a",
+    "url": "/img/wiifittrainer-ssbu.c366f8b0.png"
+  },
+  {
+    "revision": "b2317412558350be772302fe2d6232ae",
+    "url": "/img/wolf-ssbu.b2317412.png"
+  },
+  {
+    "revision": "f17c62e2aa5485642ffd70a439a5cc65",
+    "url": "/img/world-flags-globe.f17c62e2.png"
+  },
+  {
+    "revision": "cfbbbb39d5ad420e773d5e3a2d80ee1b",
+    "url": "/img/yoshi-kart.cfbbbb39.png"
+  },
+  {
+    "revision": "9c9af558f1ce260031fadd2e37671ccf",
+    "url": "/img/yoshi-ssbu.9c9af558.png"
+  },
+  {
+    "revision": "b710928db203a2db0c0394c067f46b1b",
+    "url": "/img/yoshi.b710928d.png"
+  },
+  {
+    "revision": "b91675e55d0cb27e697cf797ec574381",
+    "url": "/img/younglink-ssbu.b91675e5.png"
+  },
+  {
+    "revision": "7790a0386b8fc173321654cdcea66e3f",
+    "url": "/img/zelda-ssbu.7790a038.png"
+  },
+  {
+    "revision": "bbf03a682e6df0697098165c8e5f3712",
+    "url": "/img/zerosuitsamus-ssbu.bbf03a68.png"
+  },
+  {
+    "revision": "cd71e3af94d74a3677c08c083b4279c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90d9e67d4b12f062323",
+    "url": "/js/app.8a4e5051.js"
+  },
+  {
+    "revision": "d8118855ae774300e717",
+    "url": "/js/chunk-vendors.6e5909a0.js"
+  },
+  {
+    "revision": "9cf6ef03a14462077cb6474e022efad3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "98ee0559abca401513ca8b52e7b78916",
+    "url": "/proxy.php"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
