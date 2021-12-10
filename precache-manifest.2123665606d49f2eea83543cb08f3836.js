20602fc1ee52bd1d52c13f03b6f3c8035942cf31
GreatWizard <GreatWizard@users.noreply.github.com>
2021-12-10 12:41:20 +0000

    deploy: 8d2e80590693e7c2cbdb4381f7f2f9129de4b855

--- precache-manifest.2123665606d49f2eea83543cb08f3836.js
+++ precache-manifest.2123665606d49f2eea83543cb08f3836.js
@@ -660,7 +660,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/manifest.json"
   },
   {
-    "revision": "98ee0559abca401513ca8b52e7b78916",
+    "revision": "1069fd39b849c61c17182b6f3e9aed52",
     "url": "/proxy.php"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c5a6e3dee19860b6feaa429b4c78fde8.js"
+  "/precache-manifest.2123665606d49f2eea83543cb08f3836.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "lan-play-status"});
