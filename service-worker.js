65a9662e4a6978f9fd80dbded893911902dca59b
GreatWizard <GreatWizard@users.noreply.github.com>
2022-05-29 03:37:42 +0000

    deploy: 6a382e95c8db16dbb20e20a1a64a95f07343d259

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9027e4bd1d9c0befb7801a291a8c402.js"
+  "/precache-manifest.b5d86510de4df1649d4f249f8ec868f2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "lan-play-status"});
