696adf10418935cc45278f64dfeae17cc84fe2b0
GreatWizard <GreatWizard@users.noreply.github.com>
2023-11-20 02:33:01 +0000

    deploy: bb41ef54e5b03854b7dfeb525c0e5ceeaeea9616

--- precache-manifest.5defc8a45d883d94527869e5cb7cd7f3.js
+++ precache-manifest.5defc8a45d883d94527869e5cb7cd7f3.js
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/browserconfig.xml"
   },
   {
-    "revision": "39bdbcdcb125a93cb77c",
+    "revision": "f5dce3478e70a4b9b1ed",
     "url": "/css/app.e2a63a8d.css"
   },
   {
@@ -656,12 +656,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/zerosuitsamus-ssbu.bbf03a68.png"
   },
   {
-    "revision": "d1c4980fad3ebb0797d734b8f1a6c232",
+    "revision": "9f68b5fa83ad8ad9d8bf0e4939db7e34",
     "url": "/index.html"
   },
   {
-    "revision": "39bdbcdcb125a93cb77c",
-    "url": "/js/app.ce21b195.js"
+    "revision": "f5dce3478e70a4b9b1ed",
+    "url": "/js/app.9bd9e591.js"
   },
   {
     "revision": "4cef4ed8e5e692c54f02",
