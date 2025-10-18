5a0e4730139bd64569f6827b35ef1106d4625911
Guillaume Gérard <guillaume.gerard88@gmail.com>
2025-10-18 10:54:58 +0200

    fix: games script

--- generate-games.sh
+++ generate-games.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p tmp
-curl http://nswdb.com/xml.php -o tmp/games.nswdb.xml
-curl http://tinfoil.media/Title/ApiJson/ -o tmp/games.tinfoil.json
+curl --location http://nswdb.com/xml.php -o tmp/games.nswdb.xml
+curl --location http://tinfoil.media/Title/ApiJson/ -o tmp/games.tinfoil.json
 ./convert-games.js
